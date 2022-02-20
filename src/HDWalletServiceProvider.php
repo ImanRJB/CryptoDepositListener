e6f2f0dcf9ab5972ea89f016b925372cfc4362f3
Iman Rajabi <iman.rjb@gmail.com>
2022-02-20 12:01:23 +0330

    Initial commit

---
+++ src/HDWalletServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DepositListener\Src;
+
+use Illuminate\Support\ServiceProvider;
+
+class HDWalletServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // For load config files
+        if (file_exists(__DIR__ . '/../src/config/hd-wallet.php')) {
+            $this->mergeConfigFrom(__DIR__ . '/../src/config/hd-wallet.php', 'hd-wallet');
+        }
+
+        $this->app->alias(\HdWallet\Src\Services\AddressGenerator\AddressGenerator::class, 'AddressGenerator');
+        $this->app->alias(\HdWallet\Src\Services\PrivateKeyGenerator\PrivateKeyGenerator::class, 'PrivateKeyGenerator');
+
+        $this->app->register(\HdWallet\Src\Services\AddressGenerator\AddressGeneratorServiceProvider::class);
+        $this->app->register(\HdWallet\Src\Services\PrivateKeyGenerator\PrivateKeyGeneratorServiceProvider::class);
+    }
+}
